dc47479232d6f999f61c75c10f5c75796235ee1b
Tarun Atreya <53280371+Tarun-atreya@users.noreply.github.com>
2024-05-02 13:45:49 -0400

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
